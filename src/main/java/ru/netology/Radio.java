c954121f3da8dfd8339351b6056433ffc0d85787
EkaterinaAsanova <u-kat@mail.ru>
2025-04-22 22:44:07 +0500

    code reformat 2

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,8 +1,7 @@
 package ru.netology;
 
 
-public class Radio
-{
+public class Radio {
     private int maxNumberRadioStation;
     private int minNumberRadioStation = 0;
     private int maxSoundVolume = 100;
@@ -10,93 +9,71 @@ public class Radio
     private int currentRadioStationNumber;
     private int currentSoundVolume;
 
-    public Radio(int quantityStation)
-    {
-      maxNumberRadioStation = quantityStation -1;
+    public Radio(int quantityStation) {
+        maxNumberRadioStation = quantityStation - 1;
     }
 
-    public Radio()
-    {
-      maxNumberRadioStation = 10;
+    public Radio() {
+        maxNumberRadioStation = 10;
     }
 
-    public int getCurrentRadioStationNumber()
-    {
-      return currentRadioStationNumber;
+    public int getCurrentRadioStationNumber() {
+        return currentRadioStationNumber;
     }
 
-    public int getMaxNumberRadioStation()
-    {
+    public int getMaxNumberRadioStation() {
         return maxNumberRadioStation;
     }
 
-    public void setCurrentRadioStationNumber(int newCurrentRadioStationNumber)
-    {
-      if (newCurrentRadioStationNumber > maxNumberRadioStation)
-      {
-        return;
-      }
-      if (newCurrentRadioStationNumber < minNumberRadioStation)
-      {
-        return;
-      }
-      currentRadioStationNumber = newCurrentRadioStationNumber;
+    public void setCurrentRadioStationNumber(int newCurrentRadioStationNumber) {
+        if (newCurrentRadioStationNumber > maxNumberRadioStation) {
+            return;
+        }
+        if (newCurrentRadioStationNumber < minNumberRadioStation) {
+            return;
+        }
+        currentRadioStationNumber = newCurrentRadioStationNumber;
     }
 
-    public void next()
-    {
-        if (currentRadioStationNumber < maxNumberRadioStation)
-        {
+    public void next() {
+        if (currentRadioStationNumber < maxNumberRadioStation) {
             currentRadioStationNumber = currentRadioStationNumber + 1;
-        }
-        else
-        {
+        } else {
             currentRadioStationNumber = minNumberRadioStation;
         }
     }
-    public void prev()
-    {
-      if (currentRadioStationNumber > minNumberRadioStation)
-      {
-          currentRadioStationNumber = currentRadioStationNumber - 1;
-      }
-      else
-      {
-          currentRadioStationNumber = maxNumberRadioStation;
-      }
+
+    public void prev() {
+        if (currentRadioStationNumber > minNumberRadioStation) {
+            currentRadioStationNumber = currentRadioStationNumber - 1;
+        } else {
+            currentRadioStationNumber = maxNumberRadioStation;
+        }
     }
 
-    public int getCurrentSoundVolume()
-    {
-      return currentSoundVolume;
+    public int getCurrentSoundVolume() {
+        return currentSoundVolume;
     }
 
-    public void setCurrentSoundVolume(int newCurrentSoundVolume)
-    {
-      if (newCurrentSoundVolume < minSoundVolume)
-      {
-        return;
-      }
-      if (newCurrentSoundVolume > maxSoundVolume)
-      {
-        return;
-      }
-      currentSoundVolume = newCurrentSoundVolume;
+    public void setCurrentSoundVolume(int newCurrentSoundVolume) {
+        if (newCurrentSoundVolume < minSoundVolume) {
+            return;
+        }
+        if (newCurrentSoundVolume > maxSoundVolume) {
+            return;
+        }
+        currentSoundVolume = newCurrentSoundVolume;
     }
 
-    public void increaseSound()
-    {
-      if (currentSoundVolume < maxSoundVolume)
-      {
-          currentSoundVolume = currentSoundVolume + 1;
-      }
+    public void increaseSound() {
+        if (currentSoundVolume < maxSoundVolume) {
+            currentSoundVolume = currentSoundVolume + 1;
+        }
     }
 
-    public void reducingSoundLevel()
-    {
-      if (currentSoundVolume > minSoundVolume)
-      {
-        currentSoundVolume = currentSoundVolume - 1;
-      }
+    public void reducingSoundLevel() {
+        if (currentSoundVolume > minSoundVolume) {
+            currentSoundVolume = currentSoundVolume - 1;
+        }
     }
 }
